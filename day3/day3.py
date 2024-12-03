44d95db58a4c1b9bf4cb6091f10f5725a72deb9b
studiobigli <acuddlytoy@googlemail.com>
2024-12-03 20:36:01 +0000

    Day 3 completed

--- day3/day3.py
+++ day3/day3.py
@@ -1,25 +1,42 @@
-def multiply():
+def do_or_dont():
+    dey_do_doh_dont_dey_doh: bool = True
+    fine_text_list: list = []
     multiply_list: list = []
-    
+
     with open('day3/day3.txt', 'r') as f:
-        split_by_mul: list = f.read().split("mul(")
+        text_input = f.read()
+    
+    while text_input:
+        if dey_do_doh_dont_dey_doh:
+            fine_text_list.append(text_input.partition("don't()")[0])
+            text_input = text_input.partition("don't()")[2]
+            dey_do_doh_dont_dey_doh = False
 
+        elif not dey_do_doh_dont_dey_doh:
+            text_input = text_input.partition("do()")[2]
+            dey_do_doh_dont_dey_doh = True
 
-        for bad_string in split_by_mul:
-            new_number: int = 0
-            if "," in bad_string[1:4]:
-                second_bad_string: list = bad_string.split(")")
+        else:
+            break
+    
+    fine_text: str = "".join(fine_text_list)
+    split_by_mul: list = fine_text.split("mul(")
 
-                third_bad_string = second_bad_string[0].partition(",")
-                
-                try:
-                    new_number = int(third_bad_string[0])*int(third_bad_string[2])
-                except:
-                    print("bad multi")
-                finally:
-                    print(f"{second_bad_string[0]=}, {third_bad_string=}, {new_number=}")
-                    multiply_list.append(new_number)
+    for bad_string in split_by_mul:
+        new_number: int = 0
+        
+        if "," in bad_string[1:4]:
+            second_bad_string: list = bad_string.split(")")
+            third_bad_string = second_bad_string[0].partition(",")
+            
+            try:
+                new_number = int(third_bad_string[0])*int(third_bad_string[2])
+            except:
+                continue
+            finally:
+                multiply_list.append(new_number)
+
     return sum(multiply_list)
 
 if __name__ == "__main__":
-    print(multiply())
+    print(do_or_dont())
