acd85ae3e3640e329d796ec0c04ef2c7546de930
studiobigli <acuddlytoy@googlemail.com>
2024-12-06 17:36:52 +0000

    Day 5 complete

--- day5/day5.py
+++ day5/day5.py
@@ -13,11 +13,11 @@ def parse_input():
 
 def part1():
     rules, updates = parse_input()["rules"], parse_input()["updates"]
+    unordered_updates = []
     count = int()
 
     for update in updates:
         add_middle_page = True
-
         update_breakdown = {x: y for x,y in enumerate(update)}
         for num in update_breakdown.values():
             for rule in rules:
@@ -25,17 +25,43 @@ def part1():
                     if rule[0] in update_breakdown.values() and rule[1] in update_breakdown.values():
                         if update.index(rule[0]) > update.index(rule[1]):
                             add_middle_page = False
-
         if add_middle_page:
             count += int(update[int((len(update) -1)/2)])
+        else:
+            unordered_updates.append(update)
+        
+    return {"count": count, "unordered_updates": unordered_updates}
+                            
+def part2():
+    rules, updates = parse_input()["rules"], part1()["unordered_updates"]
+    count, checklist = int(), list()
+
+    while updates:
+        for update in updates:
+            check_update = [x for x in update]
+            append_triggered = False
+            update_breakdown = {x: y for x,y in enumerate(update)}
+            for num in update_breakdown.values():
+                for rule in rules:
+                    if num in rule:
+                        if rule[0] in update_breakdown.values() and rule[1] in update_breakdown.values():
+                            if update.index(rule[0]) > update.index(rule[1]):
+                                update.append(update.pop(update.index(rule[1])))
+                                append_triggered = True
+
+            if append_triggered:
+                break
+            if check_update == update:
+                checklist.append(updates.pop(updates.index(update)))
+    
+    for update in checklist:
+        count += int(update[int((len(update) -1)/2)])
 
     return count
-                            
-                    
-                
-                    
+
 
 
 
 if __name__ == "__main__":
-    print(f"Day 5 Part 1 Answer: {part1()}")
+    print(f"Day 5 Part 1 Answer: {part1()["count"]}")
+    print(f"Day 5 Pat 2 Answer: {part2()}")
